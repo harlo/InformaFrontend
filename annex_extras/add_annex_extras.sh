36a144645601df1a1412c2b01677db818ba173fd
harlo <harlo.holmes@gmail.com>
2014-03-27 11:07:27 -0400

    config

--- annex_extras/add_annex_extras.sh
+++ annex_extras/add_annex_extras.sh
@@ -1,7 +1,23 @@
 #! /bin/bash
+OLD_DIR=`pwd`
 ANNEX_ROOT=$1
 
 git clone git@github.com:FFmpeg/FFmpeg.git $ANNEX_ROOT/lib/FFmpeg
+cd $ANNEX_ROOT/lib/FFmpeg
+git submodule update --init --recursive
+git checkout master
+
 git clone git@github.com:harlo/Jpeg-Redaction-Library.git $ANNEX_ROOT/lib/jpeg-redaction-library
+cd $ANNEX_ROOT/lib/jpeg-redaction-library
+git submodule update --init --recursive
+git checkout master
+
 git clone git@github.com:n8fr8/JavaMediaHasher.git $ANNEX_ROOT/lib/JavaMediaHasher
+cd $ANNEX_ROOT/lib/JavaMediaHasher
+git submodule update --init --recursive
+git checkout master
+
 git clone git@github.com:isislovecruft/python-gnupg.git $ANNEX_ROOT/lib/python-gnupg
+cd $ANNEX_ROOT/lib/python-gnupg
+git submodule update --init --recursive
+git checkout master
