64d53b5a08357b8e09cb38da4f568b26e0c7bfd5
harlo <harlo.holmes@gmail.com>
2014-06-21 11:22:25 -0400

    new setup and collection stub

--- setup.sh
+++ setup.sh
@@ -1,26 +1,14 @@
 #! /bin/bash
-OLD_DIR=`pwd`
+THIS_DIR=`pwd`
 
-echo "**************************************************"
-echo "************** INFORMACAM SETUP **************"
-
-mkdir .users
-mkdir conf/.gpg
-
-cp conf/informacam.secrets.json.example conf/informacam.secrets.json
+pip install --upgrade fabric
 
 cd lib/Frontend
-./setup.sh $OLD_DIR ~/.ssh "ec2-54-83-176-172.compute-1.amazonaws.com" 8888 false
+./setup.sh $THIS_DIR/conf/informacam.secrets.json
 
-cd web
-ln -s $OLD_DIR/web/ extras
+cd $THIS_DIR/web
+ln -s $THIS_DIR/web/ extras
 
-cd ../SyncTasks
-ln -s $OLD_DIR/SyncTasks/* .
-
-#cd $OLD_DIR/lib/python-gnupg
-#make install
-
-cd $OLD_DIR
-#pip install --upgrade -r requirements.txt
+cd $THIS_DIR
+pip install --upgrade -r requirements.txt
 python informa_frontend.py -firstuse
