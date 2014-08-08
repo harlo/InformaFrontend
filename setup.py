6f7ea6f34592ab64348fa0ea95452804e1797223
harlo <harlo@guardianproject.info>
2014-08-08 12:56:26 +0000

    chiseling down the setup script

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 import os, json
 from sys import exit
-from farbic.operations import prompt
 
 from lib.Frontend.lib.Core.Utils.funcs import generateNonce
-from conf import CONF_ROOT
+from conf import CONF_ROOT, INFORMA_CONF_ROOT
 
 if __name__ == "__main__":
 	try:
-		with open(os.path.join(CONF_ROOT, "unveillance.secrets.json", 'rb') as CONF:
+		with open(os.path.join(CONF_ROOT, "unveillance.secrets.json"), 'rb') as CONF:
 			config = json.loads(CONF.read())
 	except Exception as e:
 		print "NO CONF?"	
@@ -17,12 +16,12 @@ if __name__ == "__main__":
 	else:
 		default_home_mime_types = ["image/jpeg", "video/x-matroska"]
 	
-	with open(os.path.join(COMPASS_CONF_ROOT, "compass.init.json"), 'wb+') as WEB:
+	with open(os.path.join(INFORMA_CONF_ROOT, "informacam.init.json"), 'wb+') as WEB:
 		WEB.write(json.dumps({
 			'web' : {
 				'BATCH_SALT' : generateNonce(),
 				'DEFAULT_HOME_MIME_TYPES' : default_home_mime_types
 			}
-		})
+		}))
 		
 	exit(0)
--- setup.sh
+++ setup.sh
@@ -3,10 +3,13 @@ THIS_DIR=`pwd`
 
 if [ $# -eq 0 ]
 then
-	WITH_CONFIG=$THIS_DIR/conf/informacam.secrets.json
+	echo "{}" > $THIS_DIR/lib/Frontend/conf/informa.secrets.json
+	WITH_CONFIG=$THIS_DIR/lib/Frontend/conf/informa.secrets.json
 else
 	WITH_CONFIG=$1
 fi
+
+mkdir conf
 
 cd lib/Frontend
 ./setup.sh $WITH_CONFIG
@@ -14,11 +17,11 @@ cd lib/Frontend
 cd $THIS_DIR
 python setup.py
 
-cd web
+cd $THIS_DIR/lib/Frontend/web
 ln -s $THIS_DIR/web/ extras
 
 cd $THIS_DIR
-chmod 0400 conf/compass.init.json
+chmod 0400 conf/informacam.init.json
 chmod 0400 lib/Frontend/conf/unveillance.secrets.json
 chmod 0400 lib/Frontend/conf/local.config.yaml
-python compass_frontend.py -firstuse -webapp
+python informa_frontend.py -firstuse
