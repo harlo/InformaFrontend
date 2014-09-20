1a4232c38c23be2c3415ba2a7b7ab05d4230c336
harlo <harlo.holmes@gmail.com>
2014-09-19 21:41:50 -0400

    j3m grabbing on submission load

--- web/js/models/ic_submission.js
+++ web/js/models/ic_submission.js
@@ -4,10 +4,7 @@ var InformaCamSubmission = UnveillanceDocument.extend({
 
 		if(this.get('data').j3m_id) {
 			try {
-				var j3m = new InformaCamJ3M(doInnerAjax("documents", "post", {
-					"_id" : this.get('data').j3m_id,
-					"doc_type" : "ic_j3m"
-				}, null, false).data);
+				var j3m = new InformaCamJ3M(this.getChildAsset(this.get('data').j3m_id));
 			} catch(err) {
 				console.warn(err);
 				console.warn("Could not get j3m data for document " + this.get('data')._id);
@@ -17,5 +14,8 @@ var InformaCamSubmission = UnveillanceDocument.extend({
 			this.set('j3m', j3m);
 			console.info("J3M for document " + this.get('data')._id + " found.");
 		} else { console.info("No J3M document " + this.get('data')._id); }
+	},
+	getChildAsset: function(_id) {
+		return UnveillanceDocument.prototype.getChildAsset.apply(this, [_id, "ic_j3m"]);
 	}
 });
