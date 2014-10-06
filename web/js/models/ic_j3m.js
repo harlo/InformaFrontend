fd4f4d22f5df416b8343f079c0936045504cd643
megabulk <megabulk@grundig9000.com>
2014-10-06 10:08:33 -0700

    beginning display of header data from j3m

--- web/js/models/ic_j3m.js
+++ web/js/models/ic_j3m.js
@@ -1,5 +1,9 @@
 var InformaCamJ3M = Backbone.Model.extend({
 	constructor: function() {
 		Backbone.Model.apply(this, arguments);
+	},
+	getHeader: function() {
+		var headerData = doInnerAjax("j3mheader/" + this.get("media_id"), "get", {}, null, false);
+		return headerData.data;
 	}
 });
