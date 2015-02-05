5e0e31536eabd42fcf311175b932d1436e83639a
jonny <megabulk@grundig9000.com>
2015-02-04 23:55:47 -0800

    beginning viz of TSV download function -- hopelessly broken, coming along

--- web/js/viz/ic_tsv_download.js
+++ web/js/viz/ic_tsv_download.js
@@ -10,28 +10,35 @@ app.InformaCamTSVDownloadView = Backbone.View.extend({
 //		this.header.get('appendedUserData').bind('change', this.render, this);
 		this.header.get('documentWrapper').bind('change', this.render, this);
 		
-		this.dataSets = {DocumentWrapper: ['dateAddedFormatted', 'j3m_id']};
+		this.timestamped.get('accelerometer').bind('change', this.render, this);
+		
+		this.dataSetsHeader = [{DocumentWrapper: ['dateAddedFormatted', 'j3m_id']}, ];
+		this.timestampedHeader = [{Accelerometer: ['acc_x', 'acc_y', 'acc_x', ]}, ];
 	},
 	render: function(model) {
 		var div_id = model.urlRoot.substring(1);
 		var json = model.toJSON().data;
 		
-		if (div_id == 'DocumentWrapper') {
-			json.dateAddedFormatted =  moment(Number(json.date_added)).format("MM/DD/YYYY HH:mm:ss");
+		$c(model);
+		$c(div_id);
+		$c(json);
+		if (div_id in this.dataSetsHeader) {
+		
+			if (div_id == 'DocumentWrapper') {
+				json.dateAddedFormatted =  moment(Number(json.date_added)).format("MM/DD/YYYY HH:mm:ss");
+			}
+
+			_.each(this.dataSetsHeader[div_id], function(d) {
+				cellIndex = $('#header_tsv th#tsv_' + d).index();
+				cell = $('#header_tsv tbody td:nth-child(' + (cellIndex + 1) + ')');
+				cell.html(json[d]);
+			});
+		} else if (div_id in this.timestampedHeader) {
+			var xx = model.get("values");
+			$c('values');
+			$c(xx);
 		}
 
-		$c('render ' + div_id);
-		$c(json);
-//		$c(this.dataSets[div_id]);
-		
-		
-		
-		_.each(this.dataSets[div_id], function(d) {
-			$c('d ' + d);
-			$c(json[d]);
-		});
-
-//		html = Mustache.to_html(this.template, json);
 		this.$el.addClass("rendered");
 		return this;
 	},
