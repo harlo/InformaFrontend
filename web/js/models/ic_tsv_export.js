02b72fea058c2a0df8c0e7424c8e9177604524b2
jonny <megabulk@grundig9000.com>
2015-02-08 19:37:26 -0800

    Backbone collection, model, row view, and table view for Header data, take 2

--- web/js/models/ic_tsv_export.js
+++ web/js/models/ic_tsv_export.js
@@ -25,12 +25,21 @@ jQuery(document).ready(function($) {
 	});
 
 	app.TSVHeaderDataRowView = Backbone.View.extend({
+		render: function() {
+			this.html = 'HTML';
+		}
 	});
 
+	//collection view
 	app.TSVHeaderDataTableView = Backbone.View.extend({
 		el: '#ic_TSV_header_data',
 		initialize: function() {
-			this.views = [];
+			this.rows = [];
+
+			this.listenTo(this.collection, 'add', function() {
+				$c('add headerDataCollection');
+				this.rows.push(new app.TSVHeaderDataRowView);
+			});
 		},
 		render: function() {
 		},
@@ -49,9 +58,9 @@ jQuery(document).ready(function($) {
 			*/
 			
 			this.listenTo(this.collection.headerDataCollection, 'add', function() {
-				$c('add headerDataCollection');
-				this.TSVHeaderDataTableView.views.push(new app.TSVHeaderDataRowView);
-				this.TSVHeaderDataTableView.render();
+//				$c('add headerDataCollection');
+//				this.TSVHeaderDataTableView.views.push(new app.TSVHeaderDataRowView);
+//				this.TSVHeaderDataTableView.render();
 			});
 
 		},
@@ -66,6 +75,62 @@ jQuery(document).ready(function($) {
 	});
 	
 	
-	var ddd = new app.InformaCamTSVDownloadView;
-	ddd.add({id: '385a36afc6c868b1b4f9144339622602'});	
+//	var ddd = new app.InformaCamTSVDownloadView;
+//	ddd.add({id: '385a36afc6c868b1b4f9144339622602'});
+	
+	
+	
+//TAKE 2
+//https://bardevblog.wordpress.com/2012/12/13/re-learning-backbone-js-nested-views/
+	var Datasets = Backbone.Collection.extend({
+		model: app.InformaCamDocumentWrapper
+	});
+	
+	var TableView = Backbone.View.extend({
+		el: "#ic_search_results_holder",  //the view should be decoupled from the DOM, but for this example this will do.
+		//collection:  This will be passed in during initialization
+		initialize: function () {
+			this.views = [];
+			this.listenTo(this.collection, "add", function(model, collection, options) {
+				$c('add');
+				$c(model);
+				this.views.push(new TableRowView({ model: model, parentView: this }));
+			});
+		},
+		render: function() {
+			$c('TableView render');
+			_.each(this.views, function(view) {
+				view.render();
+			}, this);
+		},
+	});
+
+	var TableRowView = Backbone.View.extend({
+		//el: since we're setting the tagName, we don't need set the el
+		tagName: "div",
+        className: "tableRow",
+		initialize: function (options) {
+			this.model.on("change", this.modelChanged, this);
+			this.parentView = options.parentView;
+			$c(options.parentView);
+		},
+		render: function () {
+			var outputHtml = this.model.toJSON().data;
+			$c(outputHtml.j3m_id);
+			this.$el.html(outputHtml.date_added);
+			return this;
+		},
+		modelChanged: function (model, changes) {
+		//	$c(model);
+			var json = model.toJSON().data;
+			console.log("modelChanged: " + json.date_added);
+			this.parentView.render();
+		},
+	});
+	
+	var dataset = new Datasets();
+	var tableView = new TableView({ collection: dataset });
+	var newRow = new app.InformaCamDocumentWrapper({id: '385a36afc6c868b1b4f9144339622602'});
+	dataset.add(newRow);
+	newRow.fetch();	
 });
