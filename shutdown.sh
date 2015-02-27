9c079403f8e6811159377748c8d37f5e58e49d07
harlo <harlo.holmes@gmail.com>
2015-02-27 12:57:50 -0500

    with automatic virtualenv

--- shutdown.sh
+++ shutdown.sh
@@ -1,3 +1,21 @@
 THIS_DIR=`pwd`
+HAS_VENV=false
+
+which virtualenv
+if [ $? -eq 0 ]; then
+	file $THIS_DIR/.venv | grep "cannot open"
+	if [ $? -eq 1 ]; then
+		HAS_VENV=true
+	fi
+fi
+
+if $HAS_VENV; then
+	source $THIS_DIR/.venv/bin/activate
+fi
+
 cd lib/Frontend
 ./shutdown.sh $THIS_DIR/informa_frontend.py
+
+if $HAS_VENV; then
+	deactivate $THIS_DIR/.venv
+fi
