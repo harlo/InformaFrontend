c5fa784c4b69db240cc2de285937f1bd9d04032b
harlo <harlo@guardianproject.info>
2015-05-06 20:01:33 +0000

    conf update and pin

--- conf.py
+++ conf.py
@@ -8,4 +8,4 @@ INFORMA_CONF_ROOT = os.path.join(INFORMA_BASE_DIR, "conf")
 WEB_TITLE = "InformaCam 2.0"
 
 PERMISSIONS['upload_local'].extend([0,1,2,3])
-PERMISSIONS['upload_global'].extend([2,3])
+PERMISSIONS['upload_global'].extend([0,1,2,3])
